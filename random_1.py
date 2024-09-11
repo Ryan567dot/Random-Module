628a46712ac50ee11ffc50659b8dd9d295c16eea
Ryan567dot <itsbluey1010@gmail.com>
2024-09-11 22:07:47 +0600

    random_1.py

---
+++ random_1.py
@@ -0,0 +1,19 @@
+#Random Integer
+
+my_number = random.randint(1,10)
+
+#Random Float
+
+my_float = random.random()
+
+#Random Alphabet
+
+text = "Bangladesh"
+
+my_alpha = random.choice(text)
+print(my_alpha)
+
+#List
+
+fruits= ("Mango","Tomato","Apple","Jackfruit","Papaya")
+#print(random.choice(fruits))
